1e2dc77a9d1b683c05d02481f348ff184a93c409
Tamas Gal <tgal@km3net.de>
2020-09-22 17:51:00 +0200

    More tests for core

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,4 +1,5 @@
 import unittest
+import mock
 
 from km3db import DBManager
 from km3db.core import on_whitelisted_host, SESSION_COOKIES
@@ -18,4 +19,29 @@ class TestKM3DB(unittest.TestCase):
         result = db.get("streamds/detectors.txt")
         assert result.startswith(
             "OID\tSERIALNUMBER\tLOCATIONID\tCITY\tFIRSTRUN\tLASTRUN\nD_DU1CPPM\t2\tA00070004\tMarseille"
+        )
+
+    @mock.patch("os.path.exists")
+    @mock.patch("os.getenv")
+    @mock.patch("km3db.core.urlopen")
+    def test_request_session_cookie_from_env(
+        self, urlopen_mock, getenv_mock, exists_mock
+    ):
+        class StreamMock:
+            def read(self):
+                return b"foo"
+
+        urlopen_mock.return_value = StreamMock()
+        exists_mock.return_value = False
+        getenv_mock.side_effect = ["username", "password"]
+
+        db = DBManager()
+        cookie = db._request_session_cookie()
+
+        getenv_mock.assert_has_calls(
+            [mock.call("KM3NET_DB_USERNAME"), mock.call("KM3NET_DB_PASSWORD")]
+        )
+
+        urlopen_mock.assert_called_with(
+            "https://km3netdbweb.in2p3.fr/home.htm?usr=username&pwd=password&persist=y"
         )

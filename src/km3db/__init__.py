52a8f898ec47af8a6ffff656f187d3f4d6f0f35e
Tamas Gal <himself@tamasgal.com>
2025-03-12 11:23:02 +0100

    Fix warnings

--- src/km3db/__init__.py
+++ src/km3db/__init__.py
@@ -1,7 +1,3 @@
-from pkg_resources import get_distribution, DistributionNotFound
-
-version = get_distribution(__name__).version
-
-
+from .version import *
 from .core import DBManager, AuthenticationError
 from .tools import StreamDS, CLBMap, APIv2
